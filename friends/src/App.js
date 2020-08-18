74df7d642e3e64668fe6c2142489d07e33c0f7aa
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-18 17:23:05 -0400

    I AM THE MATRIX

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,14 +1,40 @@
-import React from 'react';
-import './App.css';
-import SignInForm from './components/SignInForm';
-import NavBar from './components/NavBar'
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import SignInForm from "./components/SignInForm";
+import NavBar from "./components/NavBar";
 
 function App() {
+
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/" />
+        )
+      }
+    />
+  );
+
   return (
     <div className="App">
       <NavBar />
-      <div style = {{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
-      <SignInForm />
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+
+
+        <PrivateRoute path='/dashboard' component={Dashboard} />
+        <Route exact path="/" component={SignInForm} />
       </div>
     </div>
   );
