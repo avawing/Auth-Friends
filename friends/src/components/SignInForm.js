c1802c70fb6acf1dc70ab27dc35ec96ab76ed91e
Aveline Wingfield <avelinewingfield@gmail.com>
2020-08-18 15:28:18 -0400

    Mostly lined up, need to pass token still

--- friends/src/components/SignInForm.js
+++ friends/src/components/SignInForm.js
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
+import {login} from '../actions'
 
 const SignInForm = (props) => {
+  const [credentials, setCredentials] = useState({});
+
+
+  const handleChange = e => {
+    setCredentials({
+      ...credentials,
+      [e.target.name]: e.target.value,
+    })
+}
+
+const authorize = e => {
+  e.preventDefault();
+  login(credentials)  
+}
+
+
   return (
-    <Form inline>
+    <Form inline onSubmit = {authorize}>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
         <Label for="exampleEmail" className="mr-sm-2">Email</Label>
-        <Input type="email" name="email" id="exampleEmail" placeholder="something@idk.cool" />
+        <Input type="text"
+            name="username"
+            value={credentials.username}
+            onChange={this.handleChange} placeholder="badassusername" />
       </FormGroup>
       <FormGroup className="mb-2 mr-sm-2 mb-sm-0">
         <Label for="examplePassword" className="mr-sm-2">Password</Label>
-        <Input type="password" name="password" id="examplePassword" placeholder="don't tell!" />
+        <Input value={credentials.password}
+            onChange={handleChange} type="password" name="password" id="examplePassword" placeholder="don't tell!" />
       </FormGroup>
       <Button>Submit</Button>
     </Form>
